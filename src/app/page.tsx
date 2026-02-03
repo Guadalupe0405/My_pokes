cfad806f763189d44d455778da577983a53d5bd4
Lupita <hemg050704@gs.utm.mx>
2026-02-03 14:11:45 -0600

    Redirigir inicio al catálogo de Pokémon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main>
-      <h1>My Pokédex</h1>
-      <Link href="/pokes">Ver Pokémon</Link>
-    </main>
-  );
+  redirect("/pokes");
 }
